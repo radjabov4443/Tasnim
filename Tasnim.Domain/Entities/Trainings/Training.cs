71310eb603063877477f7bf3d01674870bf1f0b8
Javlonbek Rajabov <radjabov377@mail.ru>
2022-03-28 19:43:29 +0500

    DOMAIN: Fill Training Entity

--- Tasnim.Domain/Entities/Trainings/Training.cs
+++ Tasnim.Domain/Entities/Trainings/Training.cs
@@ -1,6 +1,13 @@
-﻿namespace Tasnim.Domain.Entities.Trainings
+﻿using System;
+using Tasnim.Domain.Common;
+
+namespace Tasnim.Domain.Entities.Trainings
 {
-    public class Training
+    public class Training : IAuditable
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
     }
 }
